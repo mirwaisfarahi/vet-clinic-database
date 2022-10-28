119170eab3f06e2970271ef51bbe19737ca04333
Mirwais Farahi <waisfarahi@gmail.com>
2022-10-28 20:40:19 +0500

    Edit schema.sql File

--- schema.sql
+++ schema.sql
@@ -5,3 +5,11 @@ CREATE TABLE animals
     date_of_birth DATE NOT NULL, escape_attempts INT NOT NULL, neutered BOOLEAN NOT NULL, 
     weight_kg NUMERIC(5,2) NOT NULL
     );
+
+-- Create owners and species tables and alter animals table.
+CREATE TABLE owners(id serial PRIMARY KEY, full_name VARCHAR(50) NOT NULL, age INT NOT NULL);
+ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+CREATE TABLE species(id serial PRIMARY KEY, name VARCHAR(50) NOT NULL);
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owners_id INT REFERENCES owners(id);
